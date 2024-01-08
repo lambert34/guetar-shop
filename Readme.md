9877485aab5a113dfd8cda4761bb00f4bcef696a
alexL <aleksandrlambert@gmail.com>
2024-01-08 19:46:43 +0700

    добавляет спиннер загрузки

---
+++ Readme.md
@@ -0,0 +1,2 @@
+Сайт https://letscode-dev.github.io/js-guitar-shop/
+Локальный адрес http://127.0.0.1:8080/index.html
--- index.js
+++ index.js
@@ -6,12 +6,15 @@ function render() {
     productsPage.render();
 }
 
+spinnerPage.render();
+
 let CATALOG = [];
 
 fetch('server/catalog.json')
     .then(res => res.json())
     .then(body => {
         CATALOG = body;
+        spinnerPage.handleClear();
         render();
     })
 
