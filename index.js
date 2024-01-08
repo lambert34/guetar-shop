a3823f1db2e4edcd8c00c0264a14625ea469d6f9
alexL <aleksandrlambert@gmail.com>
2024-01-08 18:38:58 +0700

    создает файл json для каталога

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 function render() {
 
     const productsStore = localStorageUtil.getProducts();
+    
     headerPage.render(productsStore.lenght);
     productsPage.render();
-
 }
 
 render();
+
+let CATALOG = [];
